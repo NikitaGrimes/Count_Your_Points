f0688a494acd764a6e6bd7892cc003afc572c4d7
NikitaGrimes <nikita.rick@gmail.com>
2023-06-23 11:23:55 +0300

    catch not found 404

--- src/app/components/not-found-error/not-found-error.component.ts
+++ src/app/components/not-found-error/not-found-error.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found-error',
   templateUrl: './not-found-error.component.html',
-  styleUrls: ['./not-found-error.component.scss']
+  styleUrls: ['./not-found-error.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotFoundErrorComponent {
 
