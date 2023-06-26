da2bbb22289a8954f80e4f1a09ada6982e3f538c
NikitaGrimes <nikita.rick@gmail.com>
2023-06-26 18:51:04 +0300

    fix

--- src/app/services/game.guard.ts
+++ src/app/services/game.guard.ts
@@ -3,8 +3,6 @@ import { CanActivateFn, Router } from '@angular/router';
 import { PlayerService } from './player.service';
 
 export const gameGuard: CanActivateFn = (route) => {
-  if (!route.paramMap.get("gameType")) return inject(Router).createUrlTree(["players"]);
-  
   const gameType: number = +<string>route.paramMap.get("gameType");
   if (gameType && inject(PlayerService).getSelectedPlayersCount() >= 2) return true;
   
